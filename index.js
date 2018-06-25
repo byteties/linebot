107016bc39dbc3b5df7ade11eb559f83be58235a
chayangkoon <chayangkoondokhom@gmail.com>
2018-06-25 14:57:51 +0700

    edit reply

--- index.js
+++ index.js
@@ -1,34 +1,22 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const request = require('request')
-const AIMLParser = require('aimlparser')
-
 const app = express()
 const port = process.env.PORT || 4000
-const aimlParser = new AIMLParser({ name:'HelloBot' })
-
-aimlParser.load(['./test-aiml.xml'])
-
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
-
 app.post('/webhook', (req, res) => {
     let reply_token = req.body.events[0].replyToken
     let msg = req.body.events[0].message.text
-    aimlParser.getResult(msg, (answer, wildCardArray, input) => {
-        reply(reply_token, answer)
-    })
+    reply(reply_token, msg)
     res.sendStatus(200)
 })
-
 app.listen(port)
-
 function reply(reply_token, msg) {
     let headers = {
         'Content-Type': 'application/json',
         'Authorization': 'Bearer {2sWN7r6ryLD/Zn/jb4eli5F+RYxneBNHyFcv6cruKRKgyV9GkWDxuOYGp86fLgKEgYEnFYV67E6JR/sP2RdtJoJK3Oj9xoL7uqoEJgxA343g+HQOMg/p6TAAZgTSuVuVhHCm/abDFQiC5zZZKM6AgwdB04t89/1O/w1cDnyilFU=}'
     }
-
     let body = JSON.stringify({
         replyToken: reply_token,
         messages: [{
@@ -36,7 +24,6 @@ function reply(reply_token, msg) {
             text: msg
         }]
     })
-
     request.post({
         url: 'https://api.line.me/v2/bot/message/reply',
         headers: headers,
