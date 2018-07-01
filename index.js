b1ff62b87fa1f51f1afafa17d599e3115154458e
chayangkoon <chayangkoondokhom@gmail.com>
2018-07-01 15:57:08 +0700

    7

--- index.js
+++ index.js
@@ -18,64 +18,70 @@ function reply(reply_token, msg) {
         'Content-Type': 'application/json',
         'Authorization': 'Bearer {2sWN7r6ryLD/Zn/jb4eli5F+RYxneBNHyFcv6cruKRKgyV9GkWDxuOYGp86fLgKEgYEnFYV67E6JR/sP2RdtJoJK3Oj9xoL7uqoEJgxA343g+HQOMg/p6TAAZgTSuVuVhHCm/abDFQiC5zZZKM6AgwdB04t89/1O/w1cDnyilFU=}'
     }
-    let body = JSON.stringify({
-        replyToken: reply_token,
-        messages: [{
-            type: 'text',
-            text: ''
-        }]
-    })
+    let body
 
     if(msg === 'hello')
     {
-      body.messages = [
-            {
-              "type": "flex",
-              "altText": "This is a Flex Message",
-              "contents": {
-                "type": "bubble",
-                "body": {
-                  "type": "box",
-                  "layout": "horizontal",
-                  "contents": [
-                    {
-                      "type": "text",
-                      "text": "Hello,"
-                    },
-                    {
-                      "type": "text",
-                      "text": "World!"
+        body = JSON.stringify({
+            replyToken: reply_token,
+            messages: [
+                {
+                  "type": "flex",
+                  "altText": "This is a Flex Message",
+                  "contents": {
+                    "type": "bubble",
+                    "body": {
+                      "type": "box",
+                      "layout": "horizontal",
+                      "contents": [
+                        {
+                          "type": "text",
+                          "text": "Hello,"
+                        },
+                        {
+                          "type": "text",
+                          "text": "World!"
+                        }
+                      ]
                     }
-                  ]
+                  }
+                }
+              ]        
+        })
+    } else if(msg === 'order') {
+        body = JSON.stringify({
+            replyToken: reply_token,
+            messages: [
+                {
+                "type": "template",
+                "altText": "this is a confirm template",
+                "template": {
+                    "type": "confirm",
+                    "text": "คุณต้องการสั่งอาหารใช่ไหม ?",
+                    "actions": [
+                        {
+                          "type": "message",
+                          "label": "Yes",
+                          "text": "yes"
+                        },
+                        {
+                          "type": "message",
+                          "label": "No",
+                          "text": "no"
+                        }
+                    ]
                 }
               }
-            }
-          ]        
-    } else if(msg === 'order') {
-        body.messages = [
-            {
-            "type": "template",
-            "altText": "this is a confirm template",
-            "template": {
-                "type": "confirm",
-                "text": "คุณต้องการสั่งอาหารใช่ไหม ?",
-                "actions": [
-                    {
-                      "type": "message",
-                      "label": "Yes",
-                      "text": "yes"
-                    },
-                    {
-                      "type": "message",
-                      "label": "No",
-                      "text": "no"
-                    }
-                ]
-            }
-          }
-        ]
+            ]
+        })
     } else {
-        body.messages = [msg + ': เป็นคำสั่งที่ไม่ถูกต้อง']
+        body = JSON.stringify({
+            replyToken: reply_token,
+            messages: [{
+                type: 'text',
+                text: msg + ': เป็นคำสั่งที่ไม่ถูกต้อง'
+            }]
+        })
     }
 
     request.post({
